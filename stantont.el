8979a8ec12766d2591f7975cfd3403f29a758ecc
Stanton Teters <stantont@gmail.com>
2009-07-20 23:21:25 -0400

    Add stantont.el with username-specific customizations.

---
+++ stantont.el
@@ -0,0 +1,60 @@
+;; Custom settings for any user name of stantont
+
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+
+;; Save backups in one place
+;; Put autosave files (ie #foo#) in one place, *not*
+;; scattered all over the file system!
+(defvar autosave-dir
+  (concat "~/tmp/emacs_autosaves/" (user-login-name) "/"))
+
+(make-directory autosave-dir t)
+
+(defun make-auto-save-file-name ()
+  (concat autosave-dir
+          (if buffer-file-name
+              (concat "#" (file-name-nondirectory buffer-file-name) "#")
+            (expand-file-name
+             (concat "#%" (buffer-name) "#")))))
+
+;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
+;; list contains regexp=>directory mappings; filenames matching a regexp are
+;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
+(defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/"))
+(setq backup-directory-alist (list (cons "." backup-dir)))
+
+;; Font
+(set-face-font 'default "-apple-dejavu sans mono-medium-r-normal--12-120-72-72-m-120-iso10646-1")
+
+;; Color Themes
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
+(require 'color-theme)
+(color-theme-initialize)
+
+;; Full screen toggle
+(defun toggle-fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                         'fullboth)))
+(global-set-key (kbd "M-n") 'toggle-fullscreen)
+
+
+;; Keyboard
+ 
+;; Split Windows
+(global-set-key [f6] 'split-window-horizontally)
+(global-set-key [f7] 'split-window-vertically)
+(global-set-key [f8] 'delete-window)
+ 
+;; Some Mac-friendly key counterparts
+(global-set-key (kbd "M-s") 'save-buffer)
+(global-set-key (kbd "M-z") 'undo)
+
+
+;; Other
+ 
+(prefer-coding-system 'utf-8)
+ 
+(server-start)
+
