1c087f3cdb8112304a6d38e9ee681116bb76ee49
Stanton Teters <stantont@gmail.com>
2010-10-03 22:55:26 -0400

    Add org-mode settings.

--- stantont.el
+++ stantont.el
@@ -26,18 +26,24 @@
 ;; Font
 ;(set-face-font 'default "-apple-dejavu sans mono-medium-r-normal--12-120-72-72-m-120-iso10646-1")
 
+;; Initial frame size
+(setq initial-frame-alist '(
+                            (top . 10) (left . 10)
+                            (width . 150) (height . 55)))
+
 ;; Color Themes
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
 (require 'color-theme)
 (color-theme-initialize)
+(color-theme-zenburn)
 
 ;; Full screen toggle
-(defun toggle-fullscreen ()
+(defun mac-toggle-fullscreen ()
   (interactive)
   (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
                                            nil
                                          'fullboth)))
-(global-set-key (kbd "M-n") 'toggle-fullscreen)
+(global-set-key (kbd "M-n") 'mac-toggle-fullscreen)
 
 ;;Change font size with mouse wheel
 ;;(require 'zoom-frm)
@@ -53,9 +59,9 @@
 (global-set-key (kbd "M-s") 'save-buffer)
 (global-set-key (kbd "M-z") 'undo)
 
-
+;;
 ;; Other
- 
+;; 
 (prefer-coding-system 'utf-8)
  
 (server-start)
@@ -69,3 +75,140 @@
       (cd dir))
   (if file
       (find-file file)))
+
+;;
+;; org-mode
+;;
+;(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+(require 'org-install)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(setq org-log-done t)
+
+(setq message-mode-hook
+      (quote (orgstruct++-mode
+              (lambda nil (setq fill-column 72) (flyspell-mode 1))
+              turn-on-auto-fill
+              bbdb-define-all-aliases)))
+
+;; Make TAB the yas trigger key in the org-mode-hook and turn on flyspell mode
+(add-hook 'org-mode-hook
+          (lambda ()
+            ;; yasnippet
+            (make-variable-buffer-local 'yas/trigger-key)
+            (setq yas/trigger-key [tab])
+            (define-key yas/keymap [tab] 'yas/next-field-group)
+            ;; flyspell mode to spell check everywhere
+            (flyspell-mode 1)))
+
+;;; checkbox counts
+;;; from http://sachachua.com/blog/2008/01/outlining-your-notes-with-org/
+(defun wicked/org-update-checkbox-count (&optional all)
+  "Update the checkbox statistics in the current section.
+This will find all statistic cookies like [57%] and [6/12] and update
+them with the current numbers.  With optional prefix argument ALL,
+do this for the whole buffer."
+  (interactive "P")
+  (save-excursion
+    (let* ((buffer-invisibility-spec (org-inhibit-invisibility))
+	   (beg (condition-case nil
+		    (progn (outline-back-to-heading) (point))
+		  (error (point-min))))
+	   (end (move-marker
+		 (make-marker)
+		 (progn (or (outline-get-next-sibling) ;; (1)
+			    (goto-char (point-max)))
+			(point))))
+	   (re "\\(\\[[0-9]*%\\]\\)\\|\\(\\[[0-9]*/[0-9]*\\]\\)")
+	   (re-box
+	    "^[ \t]*\\(*+\\|[-+*]\\|[0-9]+[.)]\\) +\\(\\[[- X]\\]\\)")
+	   b1 e1 f1 c-on c-off lim (cstat 0))
+      (when all
+	(goto-char (point-min))
+	(or (outline-get-next-sibling) (goto-char (point-max))) ;; (2)
+	(setq beg (point) end (point-max)))
+      (goto-char beg)
+      (while (re-search-forward re end t)
+	(setq cstat (1+ cstat)
+	      b1 (match-beginning 0)
+	      e1 (match-end 0)
+	      f1 (match-beginning 1)
+	      lim (cond
+		   ((org-on-heading-p)
+		    (or (outline-get-next-sibling) ;; (3)
+			(goto-char (point-max)))
+		    (point))
+		   ((org-at-item-p) (org-end-of-item) (point))
+		   (t nil))
+	      c-on 0 c-off 0)
+	(goto-char e1)
+	(when lim
+	  (while (re-search-forward re-box lim t)
+	    (if (member (match-string 2) '("[ ]" "[-]"))
+		(setq c-off (1+ c-off))
+	      (setq c-on (1+ c-on))))
+	  (goto-char b1)
+	  (insert (if f1
+		      (format "[%d%%]" (/ (* 100 c-on)
+					  (max 1 (+ c-on c-off))))
+		    (format "[%d/%d]" c-on (+ c-on c-off))))
+	  (and (looking-at "\\[.*?\\]")
+	       (replace-match ""))))
+      (when (interactive-p)
+	(message "Checkbox statistics updated %s (%d places)"
+		 (if all "in entire file" "in current outline entry")
+		 cstat)))))
+(defadvice org-update-checkbox-count (around wicked activate)
+  "Fix the built-in checkbox count to understand headlines."
+  (setq ad-return-value
+	(wicked/org-update-checkbox-count (ad-get-arg 1))))
+;;;end checkbox counts
+
+;;; integration with remember-mode
+;(org-remember-insinuate)
+;;; from http://sachachua.com/blog/2008/01/outlining-your-notes-with-org/
+;(global-set-key (kbd "C-c r") 'remember)    ;; (1)
+;(add-hook 'remember-mode-hook 'org-remember-apply-template) ;; (2)
+;(setq org-remember-templates
+;      '((?n "* %U %?\n\n  %i\n  %a" "~/notes.org")))  ;; (3)
+;(setq remember-annotation-functions '(org-remember-annotation)) ;; (4)
+;(setq remember-handler-functions '(org-remember-handler)) ;; (5)
+
+
+;;;
+;;; Buffer counts
+;;; from http://sachachua.com/blog/2008/01/outlining-your-notes-with-org/
+;;;
+(defvar count-words-buffer
+  nil
+  "*Number of words in the buffer.")
+
+(defun wicked/update-wc ()
+  (interactive)
+  (setq count-words-buffer (number-to-string (count-words-buffer)))
+  (force-mode-line-update))
+
+; only setup timer once
+(unless count-words-buffer
+  ;; seed count-words-paragraph
+  ;; create timer to keep count-words-paragraph updated
+  (run-with-idle-timer 1 t 'wicked/update-wc))
+
+;; add count words paragraph the mode line
+(unless (memq 'count-words-buffer global-mode-string)
+  (add-to-list 'global-mode-string "words: " t)
+  (add-to-list 'global-mode-string 'count-words-buffer t)) 
+
+;; count number of words in current paragraph
+(defun count-words-buffer ()
+  "Count the number of words in the current paragraph."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((count 0))
+      (while (not (eobp))
+	(forward-word 1)
+        (setq count (1+ count)))
+      count)))
